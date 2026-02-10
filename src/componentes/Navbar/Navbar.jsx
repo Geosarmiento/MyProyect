be7b650ef739995c633fbd7981a130d6628c0806
Geovanny S. <jhova29@gmail.com>
2026-02-10 22:53:32 +0100

    menuMobile_ok

--- src/componentes/Navbar/Navbar.jsx
+++ src/componentes/Navbar/Navbar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import "./navbar.scss"
-import { ShoppingCart, User, Menu } from 'lucide-react';
+import { ShoppingCart, User, Menu, X, House , Mail, Store, Building2 } from 'lucide-react';
 import { useContext } from "react";
 import { CartContext } from "../../context/CartContext.jsx";
+import { useState, useEffect } from 'react';
 
 import { Link } from 'react-router-dom';
 
@@ -10,8 +11,14 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     const { totalItems, isOpen, setIsOpen } = useContext(CartContext);
 
-const { menuOpen, setMenuOpen} = useStade (true)
+    const [ menuOpen, setMenuOpen] = useState (false);
 
+    useEffect(() => {
+    document.body.style.overflow = menuOpen ? "hidden" : "auto";
+  }, [menuOpen]);
+
+
+    
     return (
         <div>
             <div className="navbarContainer">
@@ -19,12 +26,17 @@ const { menuOpen, setMenuOpen} = useStade (true)
                     <h2>MyApp</h2>
                 </div>
 
-                <nav className='mobileNav'>
-                    <Link to="/">HOME </Link>
-                    <Link to="/about">ABOUT </Link>
-                    <Link to="/shop">SHOP </Link>
-                    <Link to="/contact">CONTACT </Link>
+            
+
+                <nav className={`mobileNav ${menuOpen ? "open" : ""}`}>
+                
+                    
+                    <Link to="/" onClick={() => setMenuOpen(false)}> HOME <House/> </Link>
+                    <Link to="/about" onClick={() => setMenuOpen(false)}>ABOUT     <Building2 /></Link>
+                    <Link to="/shop" onClick={() => setMenuOpen(false)}>SHOP  <Store/></Link>
+                    <Link to="/contact" onClick={() => setMenuOpen(false)}>CONTACT <Mail/> </Link>
                 </nav>
+                {menuOpen ? <X /> : <Menu />}
 
                 <nav className='desktopNav'>
                     <Link to="/">HOME </Link>
@@ -48,11 +60,15 @@ const { menuOpen, setMenuOpen} = useStade (true)
                         <span className='totalItem'>{totalItems}</span>
                     </div>
 
-                    <div className='menu'>
+                    
 
-                        <Menu onClick={()=>setMenuOpen(!menuOpen)}
-                            color='#F9FAFB' />
-                    </div>
+                     {menuOpen ? <X  className='xMenu'
+                                     color='#F9FAFB'
+                                     onClick={() => setMenuOpen(false)}
+                                      /> : <Menu  className='menu'
+                            onClick={() => setMenuOpen(!menuOpen)}
+                            color='#F9FAFB' />}
+                    
 
                 </div>
 
