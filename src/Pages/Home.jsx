6efc39598019c139f8525848664153a531dbd44e
Geovanny S. <jhova29@gmail.com>
2026-02-11 18:22:12 +0100

    home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import "./home.scss"
 const Home = () => {
   return (
-    <div>
+    <div className='home'>
       <h1>Bienvenido a MyApp</h1>
       <p>Explora nuestra colección de camisetas de alta calidad</p>
     </div>
